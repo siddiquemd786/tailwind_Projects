b38fd292a8c3a43c8e7ebf67278d19cce76aefbf
siddiquemd786 <siddiquemd161@gmail.com>
2024-07-04 18:11:31 +0600

    solve

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+
+
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+build: {
+rollupOptions: {
+input: {
+main: resolve(__dirname, "index.html"),
+news: resolve(__dirname, "news.html"),
+},
+    },
+  },
+});
